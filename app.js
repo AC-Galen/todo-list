e60211d1854b513e25655e441ca7e030402dbc42
Galen <lottaie7359882@gmail.com>
2021-05-06 23:08:13 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 //載入express 並建構應用程式伺服器
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
 
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true})
+
+//取得資料庫連線狀態
+const db = mongoose.connection
+
+//連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+//連線成功
+db.once('open', () => {
+  console.log('mongodb connect')
+})
 //設定網頁路由
 app.get('/', (req, res) => {
   res.send('hello world')
